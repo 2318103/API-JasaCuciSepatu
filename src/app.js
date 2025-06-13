0b9203ea4d2e207991150fb80e360351da0d97ce
2318103 <firmanafandi136@gmail.com>
2025-06-13 22:21:05 +0700

    coba dokumentasi swagger

--- src/app.js
+++ src/app.js
@@ -3,12 +3,15 @@ import authRouter from './routes/auth.route.js';
 import errorHandler from './middlewares/error.js';
 import serviceRouter from './routes/service.route.js';
 import orderRouter from './routes/order.route.js';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './docs/swagger.json' with { type: 'json' };
 
 const app = express();
 
 app.use(express.json());
 
 // Routes
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/auth', authRouter);
 app.use('/service', serviceRouter);
 app.use('/order', orderRouter);
